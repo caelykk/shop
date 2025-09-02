976a5db45bf7b6f545e7fe1cde1cd4bef5a1cfa0
caelykk <caelykkaty.m@gmail.com>
2025-09-03 00:15:15 +0300

    register models in admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Category, Size, Product, ProductSize, ProductImage
 
 # Register your models here.
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
+
+class ProductSizeInline(admin.TabularInline):
+    model = ProductSize
+    extra = 1
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'color', 'price']
+    list_filter = ['category', 'color', 'product_size']
+    search_fields = ['name', 'color', 'description']
+    prepopulated_fields = {'slug': ('name',)}
+    inlines = [ProductImageInline, ProductSizeInline]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Size)
+class SizeAdmin(admin.ModelAdmin):
+    list_display = ['name']
--- main/models.py
+++ main/models.py
@@ -30,7 +30,7 @@ class Size(models.Model):
 class ProductSize(models.Model):
     product = models.ForeignKey(
         'Product', on_delete=models.CASCADE, related_name='product_size')
-    size = models.ForeignKey(Size, on_delete=models.CASCADE)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE, blank=True)
     stock = models.PositiveBigIntegerField(default=0)
 
     def __str__(self):
